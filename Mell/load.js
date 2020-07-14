462f17ac6a5cab71d5e15db83fbf22776e1163dd
marcusvfgaldino <mvgaldino60@gmail.com>
2020-07-13 21:27:25 -0300

    Return in load metod

--- Mell/load.js
+++ Mell/load.js
@@ -1,9 +1,9 @@
-const load  = function (url, atribute) {
+const load  = function (url) {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.readyState + " / " + this.status);
-            html.insert(xhttp.responseText, atribute, "beforebegin");
+            return xhttp.responseText;
          } else {
              console.log(this.readyState + " / " + this.status);
          }
